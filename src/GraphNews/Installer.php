70b3934bd07025992a1fdd31d3941a0d2ec76da0
rebangm <jp.depigny@gmail.com>
2015-08-01 22:30:17 +0200

    improve installer

--- src/GraphNews/Installer.php
+++ src/GraphNews/Installer.php
@@ -5,6 +5,7 @@
 
 namespace GraphNews;
 use Composer\Script\Event;
+use Symfony\Component\Filesystem\Exception\IOException;
 use Symfony\Component\Filesystem\Filesystem;
 
 class Installer {
@@ -23,22 +24,28 @@ class Installer {
 
     private static function install(Event $event){
         $paths = ["/../../var/cache","/../../var/log"];
-        $filesystem = new Filesystem();
-        $io = $event->getIO();
-        foreach($paths as $path){
-            $path = __DIR__ . $path;
-            if(!$filesystem->exists($path)){
-                $filesystem->mkdir($path);
-                if($filesystem->exists($path)) {
-
-                    $io->write("Create dir : ". realpath($path));
-                }else{
-                    $io->writeError("unable to create dir :".$path);
-                    $io->writeError("Create manually". $path);
+        try {
+            $filesystem = new Filesystem();
+            $io = $event->getIO();
+            foreach ($paths as $path) {
+                $path = __DIR__ . $path;
+                if (!$filesystem->exists($path)) {
+                    $filesystem->mkdir($path);
+                    if ($filesystem->exists($path)) {
+                        $filesystem->chmod($path, 0777, 0000, true);
+                        $io->write("Create dir : " . realpath($path));
+                    } else {
+                        $io->writeError("unable to create dir :" . $path);
+                        $io->writeError("Create manually" . $path);
+                    }
+                } else {
+                    $io->write("Nothing To do, everything Ok");
                 }
-            }else{
-                $io->write("Nothing To do, everything Ok");
             }
+        }catch(IOException $IOException){
+            $io->writeError($IOException->getMessage());
+        }catch(\Exception $e){
+            $io->writeError($e->getMessage());
         }
     }
 
