3e7c76c505480ccf4f46f3c7106a52e665268acc
rebangm <jp.depigny@gmail.com>
2015-08-18 12:43:46 +0200

    change brunch config to coffee, add IP to app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,11 +9,10 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
+if (!(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1','10.234.168.103')) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
+    var_dump(!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1','10.234.168.103')));
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
