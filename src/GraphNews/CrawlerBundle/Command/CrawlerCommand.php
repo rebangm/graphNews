dd3861df67b19fc3411955f2f3a23a4dc03b06fe
rebangm <jp.depigny@gmail.com>
2015-08-27 18:03:15 +0200

    create Crawler class

---
+++ src/GraphNews/CrawlerBundle/Command/CrawlerCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace GraphNews\CrawlerBundle\Command;
+
+
+use GraphNews\CrawlerBundle\GraphNewsCrawlerBundle;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use GraphNews\CrawlerBundle\Crawler;
+use GraphNews\AdminBundle\Entity\Website;
+
+class CrawlerCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('crawler:crawl')
+            ->setDescription('lancer le crawler')
+
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        //$name = $input->getArgument('name');
+        //$kernel = $this->getContainer()->get('kernel');
+        $em = $this->getContainer()->get('doctrine');
+        var_dump($em);
+        $websites = $em->getRepository('GraphNewsAdminBundle:Website')->findAll();
+        $crawler = new Crawler\Crawler($em);
+        foreach($websites as $website) {
+            $crawler->addSite($website);
+        }
+
+        $crawler->run();
+
+
+        $output->writeln("Youhou");
+    }
+
+
+}
---
+++ src/GraphNews/CrawlerBundle/Crawler/Crawler.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * 
+ * @author: Jean-Philippe Dépigny <jdepigny.ext@orange.com>
+ * Date: 27/08/2015
+ * Time: 14:17
+ */
+
+namespace GraphNews\CrawlerBundle\Crawler;
+
+
+use Cron\CronExpression;
+
+class Crawler {
+
+    protected $sitesList;
+    protected $em;
+
+    public function __construct(\Doctrine\Bundle\DoctrineBundle\Registry $em){
+        $this->sitesList = array();
+        $this->em = $em;
+    }
+
+    /**
+     * add website in list for crawling
+     * @param \GraphNews\AdminBundle\Entity\Website $website
+     */
+    public function addSite(\GraphNews\AdminBundle\Entity\Website $website){
+        try {
+            if ($this->isTimeToCrawl($website->getFrequency())) {
+                $this->sitesList[$website->getId()] = $website;
+            }
+        }catch(\Exception $e){
+
+        }
+    }
+
+    public function getSitesList(){
+        return $this->sitesList;
+    }
+
+    /**
+     * @param $frequency
+     * @return bool
+     */
+    protected function isTimeToCrawl($frequency){
+        $cron = CronExpression::factory($frequency);
+        return $cron->isDue('now');
+    }
+
+
+    public function run(){
+        foreach($this->sitesList as site){
+            
+        }
+    }
+}
